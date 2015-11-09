44fbd6fe825728f6d69fefb186729c15e2a8e6eb
antlord <antlord92@gmail.com>
2015-11-09 20:09:21 +0800

    compile script has been updated

--- compile.sh
+++ compile.sh
@@ -1,14 +1,16 @@
 echo "Compiling mercury6 package"
 
-gfortran element6.for -o element6
-gfortran close6.for -o close6
-gfortran mercury6_2.for -o mercury6
+#gfortran element6.for -o element6
+#gfortran close6.for -o close6
+#gfortran mercury6_2.for -o mercury6
 
-echo "Create sample data files? (yes/no)"
-read create_samples
+if [ $1 != '-s' ]; then
+    echo "Create sample data files? (yes/no)"
+    read create_samples
 
-if [ $create_samples = 'yes' ] ; then
-  for i in *.sample; do
-    cp $i `echo $i | sed 's/.sample//g'`
-  done
+    if [ $create_samples = 'yes' ] ; then
+      for i in *.sample; do
+        cp $i `echo $i | sed 's/.sample//g'`
+      done
+    fi
 fi
