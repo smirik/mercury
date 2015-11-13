ad6a43af0d4e9a72416bb950eb3e9a3426d4c8b1
antlord <antlord92@gmail.com>
2015-11-13 18:00:38 +0800

    creating samples has maked by default

--- compile.sh
+++ compile.sh
@@ -5,12 +5,7 @@ gfortran close6.for -o close6
 gfortran mercury6_2.for -o mercury6
 
 if [ "$1" != "-s" ]; then
-    echo "Create sample data files? (yes/no)"
-    read create_samples
-
-    if [ $create_samples = 'yes' ] ; then
-        for i in *.sample; do
-            cp $i `echo $i | sed 's/.sample//g'`
-        done
-    fi
+    for i in *.sample; do
+        cp $i `echo $i | sed 's/.sample//g'`
+    done
 fi
