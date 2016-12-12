0aee0401cb4ef645579bf095e560064eaa763d98
idovgalyov <idovgalyov@4xxi.com>
2016-12-12 16:17:53 +0300

    compile.sh update
    some changes in compile.sh
    
    Now if you want to create input data files from samples you will need to use a key "-s".
    For example: ./compile.sh -s

--- compile.sh
+++ compile.sh
@@ -4,11 +4,16 @@ gfortran element6.for -o element6
 gfortran close6.for -o close6
 gfortran mercury6_2.for -o mercury6
 
-echo "Create sample data files? (yes/no)"
-read create_samples
-
-if [ $create_samples = 'yes' ] ; then
-  for i in *.sample; do
-    cp $i `echo $i | sed 's/.sample//g'`
-  done
+if [ "$1" != "-s" ]; then
+    for i in *.sample; do
+        cp $i `echo $i | sed 's/.sample//g'`
+    done
+#else
+#    echo "Create input data files from samples? (yes/no)"
+#    read create_samples
+#    if [ $create_samples = 'yes' || $create_samples = 'y'] ; then
+#        for i in *.sample; do
+#            cp $i `echo $i | sed 's/.sample//g'`
+#        done
+#    fi
 fi
