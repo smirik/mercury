8e4f003bca7f568ee44a8f324b33409eac652c9a
idovgalyov <idovgalyov@4xxi.com>
2016-12-22 16:52:29 +0300

    runtime optimization keys
    Included -O2 keys in "gfortran" commands in "compile.sh".
    That must significantly increase the speed of integrtion.

--- clean.sh
+++ clean.sh
@@ -9,7 +9,7 @@ rm *.dmp *.clo *.out *.tmp *.aei
 echo "Delete compiled files? (yes/no)"
 read need_delete
 
-if [ $need_delete = 'yes' ]; then
+if [ $need_delete = 'yes' ] || [ $need_delete = 'y' ]; then
   echo "Compiled files are deleted"
   if [ -e 'close6' ]; then
     rm close6
@@ -25,7 +25,7 @@ fi
 echo "Delete input files? (yes/no)"
 read need_delete_input
 
-if [ $need_delete_input = 'yes' ]; then
+if [ $need_delete_input = 'yes' ] || [ $need_delete_input = 'y' ]; then
   echo "Deleting input files"
   rm *.in
 fi
--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 echo "Compiling mercury6 package"
 
-gfortran element6.for -o element6
-gfortran close6.for -o close6
-gfortran mercury6_2.for -o mercury6
+gfortran -O2 element6.for -o element6
+gfortran -O2 close6.for -o close6
+gfortran -O2 mercury6_2.for -o mercury6
 
 if [ "$1" != "-s" ]; then
     for i in *.sample; do
