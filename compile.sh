8727502ff6999c4029edb6a9d29388d3ef9d34c2
texadactyl <richard.elkins@google.com>
2018-05-21 12:24:59 -0500

    Compile only what has been changed or is dependent on a changed inclusion file

--- compile.sh
+++ compile.sh
@@ -1,10 +1,6 @@
-FFLAGS='-g -O2 -Wline-truncation -Wsurprising -Werror'
-
 echo "Compiling mercury6 package"
 
-gfortran $FFLAGS element6.for -o element6
-gfortran $FFLAGS close6.for -o close6
-gfortran $FFLAGS mercury6_2.for -o mercury6
+make all
 
 if [ "$1" != "-s" ]; then
     for i in *.sample; do
